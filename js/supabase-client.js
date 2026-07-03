e1353aa3be290f04ed383e10b6c93c3ffce60288
Roshni-Mall-19 <mca25.mall.roshni@gnims.com>
2026-07-04 01:56:53 +0530

    Update supabase-client.js

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -2,7 +2,7 @@
 // STEP: Paste your Supabase project details here.
 // Find these in Supabase Dashboard -> Project Settings -> API
 // ============================================================
-const SUPABASE_URL = "https://goerhjfqwqnaaivhyjbp.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdvZXJoamZxd3FuYWFpdmh5amJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwNjkzOTUsImV4cCI6MjA5ODY0NTM5NX0.zXp-AsbGUlUsCUnS22-AW0MA2EpUEtGFJEdutsUnXTY";
+const SUPABASE_URL = "https://project-name.supabase.co";
+const SUPABASE_ANON_KEY = "Your_Anon_Key";
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);

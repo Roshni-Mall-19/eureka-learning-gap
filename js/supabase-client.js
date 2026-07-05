cfdc1740f7b029ba52c62272e4c1db6b76d3a433
Roshni-Mall-19 <mca25.mall.roshni@gnims.com>
2026-07-05 15:50:54 +0530

    Update supabase-client.js

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -4,5 +4,4 @@
 // ============================================================
 const SUPABASE_URL = "https://goerhjfqwqnaaivhyjbp.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdvZXJoamZxd3FuYWFpdmh5amJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwNjkzOTUsImV4cCI6MjA5ODY0NTM5NX0.zXp-AsbGUlUsCUnS22-AW0MA2EpUEtGFJEdutsUnXTY";
-
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);

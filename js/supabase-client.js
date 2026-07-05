dfb46b594e4a7f255e79e6521f000b68f8d158ee
Roshni-Mall-19 <mca25.mall.roshni@gnims.com>
2026-07-05 23:29:56 +0530

    Delete js directory

--- js/supabase-client.js
+++
@@ -1,3 +0,0 @@
-const SUPABASE_URL = "https://goerhjfqwqnaaivhyjbp.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_9ATuVhRrHoSQLLk7QUJnAg_p_lZIkHO";
-const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
